bd932238aab38836ab285c78b54232c2b4c3906c
Unknown <stefan.varga98@gmail.com>
2019-05-21 13:35:14 +0200

    models created

---
+++ Project/Project/Models/Resource.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    class Resource
+    {
+        private string id { get; set; }
+        private string name { get; set; } 
+        private string description { get; set; }
+        private Type type { get; set; }
+        private ResourceFrequency frequency { get; set; }
+        private string icon { get; set; }
+        private bool renewable { get; set; }
+        private bool strategicImportance { get; set; }
+        private bool currentlyExploited { get; set; }
+        private ResourceUnit unit { get; set; }
+        private ResourcePrice price { get; set; }
+        private string dateOfDiscovery { get; set; }
+        private HashSet<Tag> tags { get; set; }
+
+        public Resource() { }
+
+        public Resource(string id, string name, string description, Type type, ResourceFrequency frequency,
+                        string icon, bool renewable, bool strategicImportance, bool currentlyExploited, ResourceUnit unit,
+                        ResourcePrice price, string dateOfDiscovery, HashSet<Tag> tags)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.type = type;
+            this.frequency = frequency;
+            this.icon = icon;
+            this.renewable = renewable;
+            this.strategicImportance = strategicImportance;
+            this.currentlyExploited = currentlyExploited;
+            this.unit = unit;
+            this.price = price;
+            this.dateOfDiscovery = dateOfDiscovery;
+            this.tags = tags;
+        }
+    };
+
+    enum ResourceFrequency
+    {
+        Infrequent, Common, Universal
+    };
+
+    enum ResourceUnit
+    {
+        Merica, Barrel, Ton, Kilogram
+    };
+
+    enum ResourcePrice
+    {
+        Dollar
+    }
+}
---
+++ Project/Project/Models/Tag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    class Tag
+    {
+        private string id { get; set; }
+        private string name { get; set; }
+        private TagColor color { get; set; }
+        private string description { get; set; }
+
+        public Tag() { }
+
+        public Tag(string id, string name, TagColor color, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+            this.description = description;
+        }
+    }
+
+    enum TagColor
+    {
+        Red, Blue, Yellow, Green, White
+    }
+}
